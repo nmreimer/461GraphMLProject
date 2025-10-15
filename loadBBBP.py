d079ac25967bc519eaafe53144129566f572baec
nmreimer <nmreimer1@gmail.com>
2025-10-14 19:37:35 -0500

    Add loadBBBP.py for loading and splitting BBBP dataset

---
+++ loadBBBP.py
@@ -0,0 +1,34 @@
+import os
+import random
+import numpy as np
+
+import torch
+from torch.utils.data import Subset
+from torch_geometric.datasets import MoleculeNet
+
+from deepchem.splits import ScaffoldSplitter
+from deepchem.data import NumpyDataset
+
+def set_seed(seed=42):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+set_seed(42)
+
+root = "data/moleculenet"
+dataset = MoleculeNet(root=root, name="BBBP")
+
+
+ids = [d.smiles for d in dataset]
+y = None
+X = ids  
+dc_ds = NumpyDataset(X=X, y=y, ids=ids)
+
+splitter = ScaffoldSplitter()   # Bemis–Murcko scaffold splitter
+train_idx, valid_idx, test_idx = splitter.split(dc_ds, frac_train=0.8, frac_valid=0.1, frac_test=0.1, seed=42)
+
+train_set = Subset(dataset, train_idx)
+val_set   = Subset(dataset, valid_idx)
+test_set  = Subset(dataset, test_idx)
